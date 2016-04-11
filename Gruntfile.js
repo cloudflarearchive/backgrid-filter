ddb8a2c6b28a9c0213cdd268eaac686792a68195
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2016-04-11 01:46:08 +0100

    Remove deprecated option

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,7 +44,6 @@ module.exports = function (grunt) {
           "title": "backgrid-filter",
           "no-source": true,
           "categories": "categories.json",
-          "warnings": "-no_doc",
           "pretty-json": true
         }
       }
